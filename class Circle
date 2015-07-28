a432ec462cb2322dae2b8fc7da38283205f639a0
githomeg <githomeg@gmail.com>
2015-07-28 17:20:40 +0300

    first commit

---
+++ class Circle
@@ -0,0 +1,13 @@
+class Circle
+@@Pi= 3.142
+def set_radius radius
+@radius=radius
+end
+def area 
+Pi*@radius*@radius
+end
+end
+
+def (method) (parameters)
+
+end
---
+++ loop.rb
@@ -0,0 +1,17 @@
+num1=0
+num2=10
+while num2 = num2-1 | |
+	do 
+end
+
+
+#while condition
+# 	statements
+#end
+
+eg
+a=1
+while a <= 10
+	puts("binary hackers")
+	a+=1	
+end
